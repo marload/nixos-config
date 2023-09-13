69e204360060c5a4bae6724b84e76346c360f4ef
marload <rladhkst8@gmail.com>
2023-09-14 01:01:31 +0900

    fix

--- darwin/casks.nix
+++ darwin/casks.nix
@@ -6,4 +6,15 @@
   "insomnia"
   "ngrok"
   "wireshark"
+  "openvpn-connect"
+  "visual-studio-code"
+  "warp"
+  
+  # Productivity Tools
+  "obsidian"
+  "slack"
+  "notion"
+
+  # Web browser
+  "arc"
 ]
