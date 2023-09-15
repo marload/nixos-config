ec45e8fe1ffe833ef5d6f9b1269426ed2bbbf861
marload <rladhkst8@gmail.com>
2023-09-15 21:56:09 +0900

    add

--- darwin/default.nix
+++ darwin/default.nix
@@ -10,7 +10,7 @@ in
 
   environment = {
     systemPackages = import ./packages.nix { inherit pkgs; };
-    shells = with pkgs; [bash zsh fish];
+    shells = with pkgs; [ fish ];
   };
 
   # Enable fonts dir
