881eb5fafdb9eec289040c3a6478dc28dbdad6a1
Wansoo Kim <rladhkstn8@gmail.com>
2023-09-19 23:01:16 +0900

    go

--- shared/home-manager.nix
+++ shared/home-manager.nix
@@ -165,6 +165,7 @@ in
     };
   };
 
+  zellij = (import ./programs/zellij) { inherit pkgs; };
   wezterm = (import ./programs/wezterm) { inherit pkgs; };
   starship = (import ./programs/starship) { inherit pkgs; };
 }
