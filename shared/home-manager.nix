f6286290f3806763b344a5242e1655f3b0d703ee
Wansoo Kim <rladhkstn8@gmail.com>
2023-09-21 14:05:40 +0900

    1password ssh host config'

--- shared/home-manager.nix
+++ shared/home-manager.nix
@@ -69,6 +69,10 @@ in
 
   ssh = {
     enable = true;
+		extraConfig = ''
+			Host *
+				IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+		'';
   };
 
   direnv = {
