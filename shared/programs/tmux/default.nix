9155d925a5028807e255232f52b059c956a21020
Wansoo Kim <rladhkstn8@gmail.com>
2023-10-09 13:21:44 +0900

    add tmux

--- shared/programs/tmux/default.nix
+++ shared/programs/tmux/default.nix
@@ -2,21 +2,22 @@
 
 {
 	enable = true;
-	terminal = "tmux-256color";
+	clock24 = true;
 	keyMode = "vi";
-  historyLimit = 50000;
-	plugins = with pkgs.tmuxPlugins; [
-		vim-tmux-navigator
-		sensible
-		yank
-		prefix-highlight
-	];
+
 	extraConfig = ''
-	  set -sg escape-time 10 # makes vim esc usable
-    new-session -s main
-		set-option -g default-terminal "tmux-256color"
-		set -g @plugin 'catppuccin/tmux'
-		set -g @plugin 'tmux-plugins/tpm'
-		set -g @catppuccin_flavour 'mocha'
+		set -g base-index 1
+		set-option -g default-terminal "screen-256color"
+		set-option -sa terminal-features ',XXX:RGB'
 	'';
+
+	plugins = with pkgs.tmuxPlugins; [
+		{
+			plugin = catppuccin;
+			extraConfig = ''
+				set -g @catppuccin_window_tabs_enabled on
+				set -g @catppuccin_flavour 'mocha'
+			'';
+		}
+	];
 }
